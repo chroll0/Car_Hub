5f1fae81b5c599ad1e91c3d19f022b81290a9d74
Saba Antadze <sabantadze@gmail.com>
2023-10-13 17:01:44 +0400

    add link icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="model-icon.png" />
       <body className="relative">
         <Navbar />
         {children}
